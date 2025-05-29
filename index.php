d1e60c08d446db7c5bef263865294c7cd521d133
jshergill <jshergill@algomau.ca>
2025-05-29 04:37:42 +0000

    modified index page

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php 
+session_start();
 require_once('user.php');
 
 $user = new User();
@@ -6,5 +7,22 @@ $user_list = $user->get_all_users();
 
 echo "<pre>";
 print_r($user_list);
-
+if (!isset($_SESSION["authenticated"]) || $_SESSION["authenticated"] != 1) {header("Location: login.php");
+exit();}
 ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Welcome</title>
+  </head>
+  <body>
+    <h1>Assignment 2</h1>
+    <p>Welcome, <?= htmlspecialchars($_SESSION['username']) ?>!</p>
+    <p>Date: <?= date("Y-m-d") ?></p>
+
+    <p><a href="logout.php">Logout</a></p>
+  </body>
+ <footer> 
+    <p> <a href = "/logout.php">Click here to logout.</a></p>
+</html>
