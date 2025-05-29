c34b22b262e9fcf1aa81036319cae05867f573ad
jshergill <jshergill@algomau.ca>
2025-05-29 02:06:27 +0000

    modified index page
    added code to index.php page.

--- index.php
+++ index.php
@@ -1,8 +1,24 @@
+<?php 
+session_start();
+ if (isset($_SESSION["authenticated"]) && $_SESSION["authenticated"] ==1) {
+
+    } else{
+    header("Location: login.php");}
+?>
+
+<!DOCTYPE html>
 <html>
   <head>
-    <title>PHP Test</title>
+    <title>COSC4806</title>
   </head>
   <body>
-    <?php echo '<p>Hello World</p>'; ?> 
 
+    <h1>Assignment 1</h1>
+    <p>Welcome, <?=$_SESSION['username']?></p>
+    <p> Date:<? echo date("Y-m-d")?></p>
+
+
+  </body>
+ <footer> 
+    <p> <a href = "/logout.php">Click here to logout.</a></p>
 </html>
