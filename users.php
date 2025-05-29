6f9561f5390f609fe64d3f0d3fea307e0186a30f
jshergill <jshergill@algomau.ca>
2025-05-29 02:46:27 +0000

    Modified users page

--- users.php
+++ users.php
@@ -1,12 +1,12 @@
 <?php
 require_once('Database.php');
+
 Class User{
   public function get_all_users(){
-    $dbh = db_connect();
-    $sql = "SELECT * FROM users";
-    $stmt = $dbh->prepare($sql);
-    $stmt->execute();
-    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $db = db_connect();
+    $statement = $db->prepare("SELECT * FROM users;");
+    $statement->execute();
+    $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
     return $rows;
   }
 }
