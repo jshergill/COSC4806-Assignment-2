c6e386494d7b6c067765d86f20af732810e2ccfb
jshergill <jshergill@algomau.ca>
2025-05-29 04:37:19 +0000

    Updated login page

--- login.php
+++ login.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Login</title>
-  </head>
-  <body>
+<head>
+  <title>Login</title>
+</head>
+<body>
 
-    <h1>Login Form</h1>
+  <h1>Login Form</h1>
 
-    <form action="/Validate.php" method="post">
-      <label for="username">Username:</label><br>
-      <input type="text" id="username" name="username" value=""><br>
+  <form action="validate.php" method="post">
+    <label for="username">Username:</label><br>
+    <input type="text" id="username" name="username" required><br>
 
-      <label for="password">Password:</label><br>
-      <input type="password" id="password" name="password" value=""><br> <br>
+    <label for="password">Password:</label><br>
+    <input type="password" id="password" name="password" required><br><br>
 
-      <input type="submit" value="Submit">
-    </form> 
+    <input type="submit" value="Login">
+  </form>
 
-  </body>
+</body>
 </html>
