1eaceb6a707bd9b5be4c5835838f2bba50549f47
jshergill <jshergill@algomau.ca>
2025-05-29 03:56:12 +0000

    updated login page's name to register page

---
+++ Register.php
@@ -0,0 +1,21 @@
+DOCTYPE html>
+<html>
+  <head>
+    <title>Login</title>
+  </head>
+  <body>
+
+    <h1>Login Form</h1>
+
+    <form action="/Validate.php" method="post">
+      <label for="username">Username:</label><br>
+      <input type="text" id="username" name="username" value=""><br>
+
+      <label for="password">Password:</label><br>
+      <input type="password" id="password" name="password" value=""><br> <br>
+
+      <input type="submit" value="Submit">
+    </form> 
+
+  </body>
+</html>
