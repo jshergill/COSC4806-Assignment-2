992e690c4b14fcdfe5f07b7e8dc535cd81b0dffe
jshergill <jshergill@algomau.ca>
2025-05-29 02:16:38 +0000

    Added config page
    Added page and code.

---
+++ Config.php
@@ -0,0 +1,8 @@
+<?php
+ define('DB_HOST',      'srp6g.h.filess.io');
+ define('DB_USERNAME',   'COSC4806_zerofollow');
+ define('DB_PASSWORD',   $_ENV['DB_PASSWORD']);
+ define('DB_DATABASE',    'COSC4806_zerofollow');
+ define('DB_PORT',       '3305');
+
+?>
